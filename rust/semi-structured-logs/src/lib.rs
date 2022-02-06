d03c3545cc87b2e6d84952d889883663b3c3b2d2
Øyvind <oyvindskaaden@gmail.com>
2022-02-06 14:57:05 +0100

    Finish rust/semi-structured-logs

--- rust/semi-structured-logs/src/lib.rs
+++ rust/semi-structured-logs/src/lib.rs
@@ -8,17 +8,18 @@ pub enum LogLevel {
     Info,
     Warning,
     Error,
+    Debug
 }
 /// primary function for emitting logs
 pub fn log(level: LogLevel, message: &str) -> String {
-    unimplemented!()
+    format!("[{}]: {}", format!("{:?}",level).to_uppercase(), message)
 }
 pub fn info(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Info, message)
 }
 pub fn warn(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Warning, message)
 }
 pub fn error(message: &str) -> String {
-    unimplemented!()
+    log(LogLevel::Error, message)
 }
